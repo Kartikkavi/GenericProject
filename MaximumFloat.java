cce7dfa6afc15d5dcf52b8e578b5c7cdc4748598
kartik <kavimandankartik33@gmail.com>
2021-05-06 21:31:52 +0530

    Added: 3 float

---
+++ MaximumFloat.java
@@ -0,0 +1,15 @@
+package geneic;
+
+public class MaximumFloat {
+	public Float getFloatMaxNum(Float x, Float y, Float z) {
+	   	if (x.compareTo(y) > 0) {
+	      	if (x.compareTo(z) > 0) {
+	         	return x;
+	      	}
+	    	}
+	      if (y.compareTo(z) > 0) {
+	        	return y;
+	      }
+	      	return z;
+	    	}
+}
---
+++ MaximumFloatTest.java
@@ -0,0 +1,37 @@
+package geneic;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import junit.framework.Assert;
+
+class MaximumFloatTest {
+
+	@Test
+	void givenFloat_WhenMaxAtFirst_ReturnSame() {
+		MaximumFloat  maxNum = new MaximumFloat();
+	        float result = maxNum.getFloatMaxNum(97.33f, 123.3f, 1.1f);
+	        Assertions.assertEquals(123.3f,result);
+	  
+		
+	}
+	@Test
+	void givenFloat_WhenMaxAtsecond_ReturnSame() {
+		MaximumFloat  maxNum = new MaximumFloat();
+	        float result = maxNum.getFloatMaxNum(123.3f,1.1f,97.33f);
+	        Assertions.assertEquals(123.3f, result);
+	  
+		
+	}
+	@Test
+	void givenFloat_WhenMaxAtThird_ReturnSame() {
+		MaximumFloat  maxNum = new MaximumFloat();
+	        float result = maxNum.getFloatMaxNum(1.1f,97.33f,123.3f);
+	        Assertions.assertEquals(123.3f, result);
+	  
+		
+	}
+
+}
